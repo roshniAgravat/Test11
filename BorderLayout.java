ea3e4e9fffd482eb513c71f17be0f94d583b3531
roshniAgravat <roshniAgravat@users.noreply.github.com>
2016-11-26 10:56:53 +0530

    Add files via upload

--- BorderLayout.java
+++ BorderLayout.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.util.*;
 import javax.swing.*;
+import java.awt.event.*;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.Toolkit;
@@ -9,12 +10,18 @@ import java.awt.Container;
 import java.awt.BorderLayout;
 import javax.swing.border.EtchedBorder;
 
-class BorderLayout1
+class BorderLayout1 implements ActionListener
 {
 	static JFrame jf = new JFrame("This is a Border Layout");
-	public static  JComponent leftside()
+	static BorderLayout1 bl=new BorderLayout1();
+	JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		JPanel panel4 = new JPanel();
+		JPanel panel5 = new JPanel();
+	public   JComponent leftside()
 	{
-		JPanel panel1 = new JPanel();
+		
 		JCheckBox jc1=new JCheckBox("WEST");
 		panel1.setLayout(new GridLayout(10,10));
 		panel1.add(jc1);
@@ -30,13 +37,28 @@ class BorderLayout1
 		JButton btn5 = new JButton("YELLOW");
 		panel1.add(btn5);
 		
+		btn1.setName("RED1");
+		btn2.setName("ORANGE");
+		btn3.setName("GREEN");
+		btn4.setName("BLUE");
+		btn5.setName("YELLOW");
+		btn1.addActionListener(new Btnclklst());
+		btn2.addActionListener(new Btnclklst());
+		btn3.addActionListener(new Btnclklst());
+		btn4.addActionListener(new Btnclklst());
+		btn5.addActionListener(new Btnclklst());
 		return panel1;
 	}
 	
 	public static void main(String [] args)
 	{
 		 
-		Toolkit tlkt = jf.getToolkit(); 
+		bl.makeFrame();
+	
+	}
+	public void makeFrame()
+	{
+	Toolkit tlkt = jf.getToolkit(); 
 		Dimension dim = tlkt.getScreenSize();
 		jf.setBounds(0,0,dim.width,dim.height);
 		BorderLayout br=new BorderLayout();
@@ -44,23 +66,59 @@ class BorderLayout1
 		Container content = jf.getContentPane();
 		content.setLayout(br);
 		 
-		content.add(leftside(),BorderLayout.WEST);
+		content.add(bl.leftside(),BorderLayout.WEST);
 		
 		JCheckBox jc1=new JCheckBox("EAST");
 		content.add(jc1,BorderLayout.EAST);
 		
-		jc1=new JCheckBox("South");
-		content.add(jc1,BorderLayout.SOUTH);
+		jc1=new JCheckBox("SOUTH");
+		panel2.add(jc1);
+	
+		content.add(panel2,BorderLayout.SOUTH);
 		
-		jc1=new JCheckBox("center");
+		jc1=new JCheckBox("CENTER");
 		content.add(jc1,BorderLayout.CENTER);
 		
-		jc1=new JCheckBox("North");
+		jc1=new JCheckBox("NORTH");
 		content.add(jc1,BorderLayout.NORTH);
 			
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setSize(800,400);
 		jf.setVisible(true);	
+	}
+	public void actionPerformed(ActionEvent event)
+	{
+		JPanel panel1 = new JPanel();
+		JComponent comp= (JComponent)event.getSource();
+		Color color;
+		System.out.println(comp.getName());
+		if(comp.getName()=="RED1")
+		{
+			color=Color.RED;
+			panel1.setBackground(color);
+		}
+		else if(comp.getName()=="ORANGE")
+		{
+			color=Color.ORANGE;
+			panel2.setBackground(color);
+		}
+		else if(comp.getName()=="GREEN")
+		{
+			color=Color.GREEN;
+			panel3.setBackground(color);
+		}
+		else if(comp.getName()=="BLUE")
+		{
+			color=Color.BLUE;
+			panel4.setBackground(color);
+		}
+		else if(comp.getName()=="YELLOW")
+		{
+			color=Color.YELLOW;
+			panel5.setBackground(color);
+		}
+		
+			
 	
 	}
 	
